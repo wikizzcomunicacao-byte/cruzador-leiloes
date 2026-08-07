18ea94e317ffc9a464810a6841db8727f8bf87b2
wikizzcomunicacao-byte <wikizzcomunicacao@gmail.com>
2026-08-07 12:36:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1384,7 +1384,7 @@ else:
                       )
 
                       response = client.models.generate_content(
-                          model="gemini-2.5-flash",
+                          model="gemini-1.5-flash",
                           contents=prompt_ia,
                       )
                       st.success("Análise Concluída:")
